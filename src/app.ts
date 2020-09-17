b8b32762d0e8533da73276b75556f705796d9a75
unknown <jedik.robert@gmail.com>
2020-09-17 16:21:11 +0300

    static

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
+class Coupon {
+    static allowed = ['Pepperoni', 'Blazing inferno'];
+    static create(percentage: number){
+        return `PIZZA_RESTAURANT_${percentage}`;
+    }
+}
+
+console.log(Coupon.create(25));
+
 interface SizesInterface {
     availableSizes: string[];
 }
