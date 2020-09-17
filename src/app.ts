04af91c8fcd467c015dabf0145f367d0345c9a42
unknown <jedik.robert@gmail.com>
2020-09-17 16:11:09 +0300

    till interface contracts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,47 @@
-console.log('Hello TypeScript!');
+interface SizesInterface {
+    availableSizes: string[];
+}
+
+abstract class Sizes implements SizesInterface {
+    constructor(protected sizes: string[]){}
+
+    set availableSizes(sizes: string[]){
+        this.sizes = sizes;
+    }
+
+    get availableSizes() {
+        return this.sizes;
+    }
+}
+
+interface PizzaInterface extends SizesInterface {
+    readonly name: string;
+    toppings: string[];
+    updateSizes(sizes: string[]): void;
+    addTopping(topping: string): void;
+}
+
+
+class Pizza extends Sizes implements PizzaInterface{
+    toppings: string[] = [];
+
+    constructor(readonly name: string, sizes: string[]) {
+        super(sizes);
+    }
+
+    public updateSizes(sizes: string[]){
+        this.sizes = sizes;
+    }
+
+    addTopping(topping: string){
+        this.toppings.push(topping);
+    }
+
+}
+
+
+
+const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+console.log(pizza.availableSizes);
+pizza.updateSizes(['large']);
+console.log(pizza.availableSizes);
